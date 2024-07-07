6d5634890f9e453c4925c59ac88dfb4d56f3ef43
howabsurd <poptanikrish1@gmail.com>
2024-07-07 11:59:24 +0530

    some more changes

--- src/controllers/groupController.js
+++ src/controllers/groupController.js
@@ -2,28 +2,26 @@ import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { groupRepository } from '../repository/groupRepository.js';
 import { groupUserRepository } from '../repository/groupUserRepository.js';
 
-export class GroupController {
-  createGroup = catchAsyncError(async (req, res) => {
+export const groupController = {
+  createGroup: catchAsyncError(async (req, res) => {
     const newGroup = await groupRepository.createGroup(req.body);
     req.body.group_id = newGroup.group_id;
     const welcomeUser = await groupUserRepository.addUser(req.body);
     return res.status(200).json({ newGroup, welcomeUser });
-  });
+  }),
 
-  deleteGroup = catchAsyncError((req, res) => {
+  deleteGroup: catchAsyncError((req, res) => {
     groupRepository.deleteGroup(req.params);
     return res.status(200).json({ message: `The Group deleted successfully` });
-  });
+  }),
 
-  addUserToGroup = catchAsyncError(async (req, res) => {
+  addUserToGroup: catchAsyncError(async (req, res) => {
     const welcomeUser = await groupUserRepository.addUser(req.body);
     return res.status(200).json({ welcomeUser });
-  });
+  }),
 
-  deleteUserFromGroup = catchAsyncError(async (req, res) => {
+  deleteUserFromGroup: catchAsyncError(async (req, res) => {
     const deleteUser = await groupUserRepository.deleteUser(req.body);
     return res.status(200).json({ deleteUser });
-  });
-}
-
-export const groupController = new GroupController();
+  }),
+};
--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,16 +1,14 @@
 import { catchAsyncError } from '../middleware/catchAsyncError.js';
 import { userRepository } from '../repository/userRepository.js';
 
-export class UserController {
-  createUser = catchAsyncError(async (req, res) => {
+export const userController = {
+  createUse: catchAsyncError(async (req, res) => {
     const newUser = await userRepository.createUser(req.body);
     return res.status(200).json({ newUser });
-  });
+  }),
 
-  deleteUser = catchAsyncError(async (req, res) => {
+  deleteUser: catchAsyncError(async (req, res) => {
     const deletedUser = await userRepository.deleteUser(req.params);
     return res.status(200).json({ message: deletedUser });
-  });
-}
-
-export const userController = new UserController();
+  }),
+};
